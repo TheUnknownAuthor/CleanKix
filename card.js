4ef9734c7cebdd844ad205dc75eacf0f41127733
TheUnknownAuthor <maxnsansom@gmail.com>
2025-05-22 20:47:52 -0400

    new

--- card.js
+++ card.js
@@ -13,7 +13,7 @@ class Card {
       '<div class="col">',
         '<div class="card" style="border: 2px solid black;">',
           '<div class="card-body">',
-            `<img src="${img}" class="card-img-top" alt="User Image">`,
+            `<img style = "width: 150px; height: 150px;" src="${img}" class="card-img-top" alt="User Image">`,
             `<h5 class="card-title">${Title}</h5>`,
             `<div class="progress" style="margin-bottom:5px;" role="progressbar" aria-label="Example with label" aria-valuenow="${prog}" aria-valuemin="0" aria-valuemax="100">`,
               `<div class="progress-bar progress-bar-striped" style="width:${prog}%;">${prog}%</div>`,
@@ -33,7 +33,7 @@ function createCard(title, id, img, prog) {
 }
 
 for (const [id, shoe] of Object.entries(shoes)) {
-  createCard(shoe.name, id, shoe.img, "50");
+  createCard(shoe.name, id, shoe.img, "50");  // shoe.img is Base64 string now
 }
 
 
