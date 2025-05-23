069a85ebc27464e57b6f60f914128f54b01ae7be
TheUnknownAuthor <83304216+TheUnknownAuthor@users.noreply.github.com>
2025-05-22 21:08:22 -0400

    Update shoes.js

--- shoes.js
+++ shoes.js
@@ -1,4 +1,4 @@
-const defaultShoes = {
+const shoes = {
     Jodran1: { name: "Jodran12", ETA: "Hello", img:"https://decantlabs.com/cdn/shop/files/IMG_1206.png?v=1740889745&width=280"},
     Yeezy: { name: "Yeezy2", ETA: "Hello", img:"https://decantlabs.com/cdn/shop/files/IMG_1206.png?v=1740889745&width=280"}
   };
